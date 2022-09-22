a82467cfa341c2230a0d84f47c0f1d2bf43fd61b
ITSC <ITSC@DESKTOP-ONE4R81>
2022-09-22 21:47:58 +0900

    .

--- src/day02/basic/MainClass15.java
+++ src/day02/basic/MainClass15.java
@@ -4,6 +4,8 @@ public class MainClass15 {
 	public static void main(String[] args) {
 		for(int i=0; i < 5; i++)
 			System.out.println(i);
+		
+		
 		int i=0;
 		while(i<5) {
 			System.out.println(i);
